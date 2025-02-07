9daee783686d8046372726451a6abfef522e6c8e
Сатбек Турганбаев <s.turganbayev@corp.mail.ru>
2025-02-07 10:44:46 +0000

    [TNTP-1495] Пример migrations: исправлена миграция, спейс создаются только на стораджах, а не на всех инстансах
    * исправлена миграция 001_test.lua в примере migrations
    * доработано описание к примеру

--- examples/migrations/cluster/migrations/scenario/001_test.lua
+++ examples/migrations/cluster/migrations/scenario/001_test.lua
@@ -11,49 +11,49 @@ local function is_storage()
 end
 
 local function apply()
-    -- создание спейсов и индексов для них
-    box.schema.space.create('projects', { if_not_exists = true })
-    box.space.projects:format({
-        { name = 'project_id', type = 'uuid' },
-        { name = 'bucket_id', type = 'unsigned' },
-        { name = 'name', type = 'string' },
-        { name = 'description', type = 'string', is_nullable = true },
-    })
-    box.space.projects:create_index('pk', { parts = {'project_id'}, if_not_exists = true})
-    box.space.projects:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
-
-    helpers.register_sharding_key('projects', {'project_id'}) -- указываем ключ шардирования для модуля CRUD
-
-    box.schema.space.create('tasks', { if_not_exists = true })
-    box.space.tasks:format({
-        { name = 'task_id', type = 'uuid' },
-        { name = 'bucket_id', type = 'unsigned' },
-        { name = 'name', type = 'string' },
-        { name = 'description', type = 'string', is_nullable = true },
-        { name = 'status', type = 'string' },
-        { name = 'project_id', type = 'uuid' },
-        { name = 'assigned_user_id', type = 'uuid', is_nullable = true },
-    })
-    box.space.tasks:create_index('pk', { parts = {'task_id'}, if_not_exists = true})
-    box.space.tasks:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
-    box.space.tasks:create_index('project_id', { parts = {'project_id'}, unique = false, if_not_exists = true})
-    box.space.tasks:create_index('assigned_user_id', { parts = {'assigned_user_id'}, unique = false, if_not_exists = true})
-
-    helpers.register_sharding_key('tasks', {'project_id'})
-
-    box.schema.space.create('users', { if_not_exists = true })
-    box.space.users:format({
-        { name = 'user_id', type = 'uuid' },
-        { name = 'bucket_id', type = 'unsigned' },
-        { name = 'name', type = 'string' },
-        { name = 'email', type = 'string', is_nullable = true },
-    })
-    box.space.users:create_index('pk', { parts = {'user_id'}, if_not_exists = true})
-    box.space.users:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
-
-    helpers.register_sharding_key('users', {'user_id'})
-
     if is_storage() then
+        -- создание спейсов и индексов для них
+        box.schema.space.create('projects', { if_not_exists = true })
+        box.space.projects:format({
+            { name = 'project_id', type = 'uuid' },
+            { name = 'bucket_id', type = 'unsigned' },
+            { name = 'name', type = 'string' },
+            { name = 'description', type = 'string', is_nullable = true },
+        })
+        box.space.projects:create_index('pk', { parts = {'project_id'}, if_not_exists = true})
+        box.space.projects:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
+
+        helpers.register_sharding_key('projects', {'project_id'}) -- указываем ключ шардирования для модуля CRUD
+
+        box.schema.space.create('tasks', { if_not_exists = true })
+        box.space.tasks:format({
+            { name = 'task_id', type = 'uuid' },
+            { name = 'bucket_id', type = 'unsigned' },
+            { name = 'name', type = 'string' },
+            { name = 'description', type = 'string', is_nullable = true },
+            { name = 'status', type = 'string' },
+            { name = 'project_id', type = 'uuid' },
+            { name = 'assigned_user_id', type = 'uuid', is_nullable = true },
+        })
+        box.space.tasks:create_index('pk', { parts = {'task_id'}, if_not_exists = true})
+        box.space.tasks:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
+        box.space.tasks:create_index('project_id', { parts = {'project_id'}, unique = false, if_not_exists = true})
+        box.space.tasks:create_index('assigned_user_id', { parts = {'assigned_user_id'}, unique = false, if_not_exists = true})
+
+        helpers.register_sharding_key('tasks', {'project_id'})
+
+        box.schema.space.create('users', { if_not_exists = true })
+        box.space.users:format({
+            { name = 'user_id', type = 'uuid' },
+            { name = 'bucket_id', type = 'unsigned' },
+            { name = 'name', type = 'string' },
+            { name = 'email', type = 'string', is_nullable = true },
+        })
+        box.space.users:create_index('pk', { parts = {'user_id'}, if_not_exists = true})
+        box.space.users:create_index('bucket_id', { parts = {'bucket_id'}, unique = false, if_not_exists = true})
+
+        helpers.register_sharding_key('users', {'user_id'})
+
         -- задаем функции, вызываемые через vshard
         -- tasks.replace_user заменяет одного пользователя на другого по всем задачам на сторадже
         -- tasks у которых assigned_user_id == user_id
